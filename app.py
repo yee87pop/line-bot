a00bb6e5f52f26e3f1edc5b79945e3dfed025d44
yee87pop <57321585+yee87pop@users.noreply.github.com>
2022-02-14 23:22:29 +0800

    crawler

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=str(event.message.text)))
+        TextSendMessage(text=str(crawler(event.message.text))))
 
 if __name__ == "__main__":
     app.run()
