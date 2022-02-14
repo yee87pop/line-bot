30052be77a9952258568910f9c2891bbf3791337
yee87pop <57321585+yee87pop@users.noreply.github.com>
2022-02-14 21:22:56 +0800

    1

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(event.message.text))
-        print(event)
+    print(event)
 
 if __name__ == "__main__":
     app.run()
