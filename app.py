47416d28140e204c826f1ca1747c713fec9f1e52
yee87pop <57321585+yee87pop@users.noreply.github.com>
2022-02-14 20:47:50 +0800

    error test

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="傻逼"))
+        TextSendMessage(text='oh man'))
 
 
 if __name__ == "__main__":
