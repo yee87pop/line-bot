1024211f107ddaa2bdff8ca8079cc8f133ffe0cc
yee87pop <57321585+yee87pop@users.noreply.github.com>
2022-02-26 20:17:05 +0800

    1

--- app.py
+++ app.py
@@ -85,9 +85,7 @@ def handle_message(event):
 def handle_message2(event):
     try:
         message_content = line_bot_api.get_message_content(event.message.id)
-        with open('./content/img.jpg', 'wb') as fd:
-            for chunk in message_content.iter_content():
-                fd.write(chunk)
+        print(message_content)
         line_bot_api.reply_message(
         event.reply_token,
         ImageSendMessage(
