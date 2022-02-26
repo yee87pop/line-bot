b6eb907a7116230388a4ad228b08774002cee56f
yee87pop <57321585+yee87pop@users.noreply.github.com>
2022-02-26 13:49:00 +0800

    image

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage, ImageSendMessage
 )
 import configparser
 
@@ -81,6 +81,20 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="聽無")) 
 
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_message2(event):
+    try:
+        line_bot_api.reply_message(
+        event.reply_token,
+        ImageSendMessage(
+        original_content_url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.greenpeace.org%2Fhongkong%2Fissues%2Fclimate%2Fupdate%2F11868%2F%25E6%25B0%25A3%25E5%2580%2599%25E5%258D%25B1%25E6%25A9%259F%25E4%25BB%25A4%25E6%25B5%25B7%25E5%2586%25B0%25E8%259E%258D%25E5%258C%2596-%25E5%258D%2597%25E6%25A5%25B5%25E7%259A%2587%25E5%25B8%259D%25E4%25BC%2581%25E9%25B5%259D%25E3%2580%258C%25E5%2586%2587%25E6%258E%259F%25E4%25BC%2581%25E3%2580%258D%25E6%2588%2596%25E6%2588%2590%2F&psig=AOvVaw2cZfhQzlRUaj21OYoa0C4t&ust=1645940876393000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCZpNzVnPYCFQAAAAAdAAAAABAD',
+        preview_image_url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fzh.wikipedia.org%2Fwiki%2F%25E4%25BC%2581%25E9%25B5%259D&psig=AOvVaw0MmCg4Z34984Zp8bQjYQKO&ust=1645940901244000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLjK7efVnPYCFQAAAAAdAAAAABAD'
+        ))        
+    except:
+        print('error')
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="error"))
 
     
 if __name__ == "__main__":
